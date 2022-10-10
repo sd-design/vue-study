ba92cf217de020220803c2518b30856e2bbac763
Alex Solovey <naghtigall@gmail.com>
2022-10-10 17:33:56 +0300

    Next lesson of Vue

--- js/app.js
+++ js/app.js
@@ -3,12 +3,18 @@ template: "#apple",
 data(){
     return {
         title: "Shabbat Shalom!",
-        href: "https://sd-design.tk/"
+        href: "https://sd-design.tk/",
+        link2: "https://media-church.ru/",
+        consoleString: ""
     }
 },
 methods:{
     handleChange(e){
        this.title = e.target.value
+    },
+    handleConsole(stringParam, event){
+        this.consoleString = stringParam
+        console.log(event)
     }
 }
 })
